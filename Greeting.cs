ef4fe66ca8963f0be793c7004aae394e10b0ea1b
Gabriel Moral <gabrielmoralortiz@gmail.com>
2018-01-14 18:41:21 +0000

    Encapsulate names behavior

--- Greeting.cs
+++ Greeting.cs
@@ -9,7 +9,6 @@ namespace greeting_kata_csharp
         private static string space = " ";
         private static string point = ".";
         private static string exclamation = "!";
-        private static string and = "and";
 
         public static string greet(params string[] namesList)
         {
@@ -27,63 +26,17 @@ namespace greeting_kata_csharp
 
         private static bool isShouting(Names names)
         {
-            return !names.IsEmpty() && names.GetFirst().Equals(names.GetFirst().ToUpper());
+            return names.IsFirstUpperCase();
         }
 
         private static string hey(Names names)
         {
-            return hello + comma + space + composeNames(names) + point;
+            return hello + comma + space + names.Compose() + point;
         }
-
-        private static string composeNames(Names names)
-        {
-            if (names.IsEmpty())
-            {
-                return noNames();
-            }
-
-            if (names.Length == 2)
-            {
-                return twoNames(names);
-            }
-
-            if (names.Length > 2)
-            {
-                return manyNames(names);
-            }
-
-            return oneName(names);
-        }
-
-        private static string manyNames(Names names)
-        {
-            string output = "";
-            for (int i = 0; i < names.Length - 1; i++)
-            {
-                output = output + names.Get(i) + comma + space;
-            }
-
-            return output + and + space + names.Get(names.Length - 1);
-        }
-
-        private static string twoNames(Names names)
-        {
-            return names.GetFirst() + space + and + space + names.GetSecond();
-        }
-
-        private static string oneName(Names names)
-        {
-            return names.GetFirst();
-        }
-
-        private static string noNames()
-        {
-            return "my friend";
-        }
-
+        
         private static string shout(Names names)
         {
-            return hello.ToUpper() + space + names.GetFirst() + exclamation;
+            return hello.ToUpper() + space + names.Compose() + exclamation;
         }
     }
 }
--- Names.cs
+++ Names.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace greeting_kata_csharp
 {
     public class Names
     {
+        private static string comma = ",";
+        private static string space = " ";
+        private static string and = "and";
+
         private readonly string[] _names;
 
         public Names(params string[] names)
@@ -9,24 +15,63 @@ namespace greeting_kata_csharp
             _names = names;
         }
 
-        public int Length { get{ return _names.Length;} }
+        public string Compose()
+        {
+            if (IsEmpty())
+            {
+                return NoNames();
+            }
 
-        public string GetFirst()
+            if (_names.Length == 2)
+            {
+                return TwoNames();
+            }
+
+            if (_names.Length > 2)
+            {
+                return ManyNames();
+            }
+
+            return FirstName();
+        }
+
+        public bool IsFirstUpperCase()
+        {
+            return  !IsEmpty() && FirstName().Equals(FirstName().ToUpper());
+        }
+
+        private  string ManyNames()
+        {
+            string output = "";
+            for (int i = 0; i < _names.Length - 1; i++)
+            {
+                output = output + _names[i] + comma + space;
+            }
+
+            return output + and + space + _names[_names.Length - 1];
+        }
+
+        private string TwoNames()
+        {
+            return FirstName() + space + and + space + SecondName();
+        }
+
+        private string NoNames()
+        {
+            return "my friend";
+        }
+
+        private string FirstName()
         {
             return _names[0];
         }
 
-        public string GetSecond()
+        private string SecondName()
         {
-             return _names[1];
+            return _names[1];
         }
 
-        internal string Get(int i)
-        {
-            return _names[i];
-         }
-
-        internal bool IsEmpty()
+        private bool IsEmpty()
         {
             return _names == null;
         }
